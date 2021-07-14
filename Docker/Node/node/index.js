77f31c453ee3d4521734262a7b4ba85d7c612198
Laila Dias <laila.fdias@hotmail.com>
2021-07-13 21:31:19 -0300

    Correção após revisão do desafio 2 nginx com node.js

--- Docker/Node/node/index.js
+++ Docker/Node/node/index.js
@@ -7,17 +7,42 @@ const config = {
     password: 'root',
     database:'nodedb'
 };
-const mysql = require('mysql')
-const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Wesley')`
+const mysql = require('mysql')
+
+connection = mysql.createConnection(config);
+
+const sql = `INSERT INTO people(name) values('Laila Dias')`
+
 connection.query(sql)
-connection.end()
+
+let table = '';
+
+connection.query("SELECT * FROM people", function (err, result, fields) {
+    if (err) throw err;
+    
+    result.forEach(element => {
+            console.log(element);
+            table += `<tr><td>${element.name}</td></tr>`
+    });
+});
+
+connection.end();
 
 
 app.get('/', (req,res) => {
-    res.send('<h1>Full Cycle</h1>')
-})
+    res.send(
+
+        `<h1>Full Cycle Rocks!</h1>`+
+        `<table>
+            <tr>
+                <td><strong>Nome</strong></td>
+            </tr>
+            ${table}
+        </table>`
+    )
+});
+
 
 app.listen(port, ()=> {
     console.log('Rodando na porta ' + port)
